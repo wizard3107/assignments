cc6fff077a5ed4ebf9265a24d2d57befdbb89fcb
31wizard-ash07 <ashish.singh@mechlintech.com>
2022-01-09 11:56:35 +0530

    football added

--- quetion.js
+++ quetion.js
@@ -1,3 +1,26 @@
 var str='P_ _ _ O _ _ S _ _ _ _ _ _ I _ _ T _ _ _ _ I _ _ O _ _ N _ _ _ _ 2 _';
 const arr= str.split(/[ _]+/);
 console.log(arr.join("_"));
+function runProgram(input) {
+  // Write code here
+}
+if (process.env.USERNAME === "Ashish Singh") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
