d3eaf4de37493186e3e4090ebe8f21d58cbbd973
wizard3107 <ashishsingh9720@gmail.com>
2022-01-14 11:55:23 +0530

    js intro completed

---
+++ course/assesment-5298-2274/agecalculator.js
@@ -0,0 +1,3 @@
+const yob=2000;
+var age=2022-yob;
+console.log(`Age:${age}`);
---
+++ course/assesment-5298-2274/index.js
@@ -0,0 +1,13 @@
+let a=10;
+let b=3;
+var add,sub,mul,div,rem,incre,decre,pow;
+console.log(`Addition: ${a+b}`);
+console.log(`Subtraction: ${a-b}`);
+console.log(`Multiplication: ${a*b}`);
+console.log(`Divison: ${(a/b).toFixed(2)}`);
+console.log(`Reminder: ${a%b}`);
+console.log(`Increment: ${++a}`);
+console.log(`Decrement: ${--b}`);
+console.log(`A raise to power B: ${a**b}`);
+console.log(`B raise to power A: ${b**a}`);
+
