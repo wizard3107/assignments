75703907e7a1b94efb599909249657b9639e9ddd
wizard3107 <ashishsingh9720@gmail.com>
2022-01-14 10:35:59 +0530

    JS Started

---
+++ course/percentage.js
@@ -0,0 +1,5 @@
+const m1=73;
+    const m2=85;
+    const m3=62;
+    let percentage = ((m1+m2+m3)/3);
+    console.log(`Percentage: ${percentage.toFixed(2)}`);
---
+++ course/pm_area.js
@@ -0,0 +1,6 @@
+const length = 17;
+const width=10;
+let perimeter= 2*(length+width);
+let area = length*width;
+console.log(`Perimeter: ${perimeter}`);
+console.log(`Area: ${area}`); 
