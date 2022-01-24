5f60d45fb5cdae9a6be1404e54e88091cdb29ab6
wizard3107 <ashishsingh9720@gmail.com>
2022-01-24 17:04:39 +0530

    stopwatch edited

--- js/cube.js
+++ js/cube.js
@@ -3,3 +3,26 @@ var cube = arr.map(function(n){
     return n ** 3;
 })
 console.log(cube);
+function runProgram(input) {
+  // Write code here
+}
+if (process.env.USERNAME === "Ashish Singh") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
